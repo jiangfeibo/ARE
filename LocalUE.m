24e770065e1e547e18ce36802b080408ca88511f
jiangfeibo <70938545+jiangfeibo@users.noreply.github.com>
2020-09-08 16:19:45 +0800

    Delete LocalUE.m

--- LocalUE.m
+++
@@ -1,7 +0,0 @@
-function [Localresut] = LocalUE(para)
-%Execute the tasks locally 
-  Localresut(1:para.UEn)=0;
-  Localresut(para.UEn+1:2*para.UEn)=para.fL;
-  Localresut(2*para.UEn+1:3*para.UEn)=para.PL;
-end
-
